ea703902753114dabbc15adb5ef6d852f561fae8
SOTA <snssota@gmail.com>
2022-07-17 14:34:27 +0900

    Reactテキストの勉強②

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
+import { ColoredMessage } from "./components/coloredMessage"
 export const App = () => {
     const onClickButton = () => {
         alert();
     }
+
+    const contentPinkStyle = {
+        color: "pink",
+        fontSize: "20px"
+    };
     return (
         <>
             {console.log("TEST")}
-            <h1>こんにちは！</h1>
-            <p>お元気ですか？</p>
+            <h1 style={{color:"red"}}>こんにちは！</h1>
+            <ColoredMessage />
+            <p style={contentPinkStyle}>お元気ですか？</p>
             <button onClick={onClickButton}>ボタン</button>
         </>
     )
---
+++ src/components/coloredMessage.jsx
@@ -0,0 +1,7 @@
+export const ColoredMessage = () => {
+    const contentStyle = {
+        color: "blue",
+        fontsize: "20px"
+    };
+    return <p style={contentStyle}>お元気ですか？</p>;
+}
